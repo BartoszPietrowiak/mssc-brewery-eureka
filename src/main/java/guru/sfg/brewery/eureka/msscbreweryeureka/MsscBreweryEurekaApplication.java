9b4a337ecea4c43721c2279d6c7b287584c1c583
bpietrowiak <bpietrowiak@consdata.pl>
2023-04-21 18:25:24 +0200

    final commit

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -3,6 +3,9 @@ package guru.sfg.brewery.eureka.msscbreweryeureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -10,6 +13,19 @@ public class MsscBreweryEurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsscBreweryEurekaApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.authorizeRequests()
+					.anyRequest()
+					.authenticated()
+					.and()
+					.httpBasic();
+		}
 	}
 
 }
